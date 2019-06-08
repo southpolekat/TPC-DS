04f07855ce343750aadfff13f060e209026ef936
wkliu <wkliu@MacBook-Pro-4.local>
2019-06-08 13:52:15 +0800

    set_compresstype

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -12,8 +12,6 @@ MULTI_USER_COUNT=$4
 SINGLE_USER_ITERATIONS=$5
 COMPRESSTYPE=$6
 
-set_compresstype $COMPRESSTYPE
-
 if [[ "$GEN_DATA_SCALE" == "" || "$EXPLAIN_ANALYZE" == "" || "$RANDOM_DISTRIBUTION" == "" || "$MULTI_USER_COUNT" == "" || "$SINGLE_USER_ITERATIONS" == "" ]]; then
 	echo "You must provide the scale as a parameter in terms of Gigabytes, true/false to run queries with EXPLAIN ANALYZE option, true/false to use random distrbution, multi-user count, and the number of sql iterations."
 	echo "Example: ./rollout.sh 100 false false 5 1"
@@ -23,6 +21,7 @@ fi
 step=ddl
 init_log $step
 get_version
+set_compresstype $COMPRESSTYPE
 
 if [[ "$VERSION" == *"gpdb"* ]]; then
 	filter="gpdb"
